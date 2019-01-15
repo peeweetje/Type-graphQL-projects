da9aa415adee5dd10cfa515b0cfe81d8530b0c03
Petra Vos <petravos70@gmail.com>
2019-01-15 12:01:44 +0100

    Add new files: login and me.Add login and me resolver to index file. Add Redis file. Add interface for Context

---
+++ src/modules/user/Me.ts
@@ -0,0 +1,16 @@
+import { Resolver, Query, Ctx } from "type-graphql";
+
+import { User } from "../../entity/User";
+import { Context } from "../../types/Context";
+
+@Resolver()
+export class MeResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() ctx: Context): Promise<User | undefined> {
+    if (!ctx.req.session!.userId) {
+      return undefined;
+    }
+
+    return User.findOne(ctx.req.session!.userId);
+  }
+}
